5553897c1e5a7e092574d470037dc45f4d1925cd
mazinalnaimat <mazinalnaimat@gmail.com>
2025-12-19 13:58:37 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/15 - Generate Binary Numbers/Generate Binary Nums Using For Loops.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _15___Generate_Binary_Numbers_Using_For_Loops
+{
+    internal class Program
+    {
+        public static List<StringBuilder> GenerateBinNum(int MaxNumber)
+        {
+            List<StringBuilder> BinNumList = new List<StringBuilder>();
+            if (MaxNumber > 0)
+            {
+                StringBuilder Number = new StringBuilder("0");
+                bool AddNum = false;
+                for (int i = 1; i <= MaxNumber; i++)
+                {
+                    AddNum = false;
+                    for (int j = Number.Length - 1; j >= 0; j--)
+                    {
+                        if (Number[j] == '0')
+                        {
+                            AddNum = true;
+                            Number[j] = '1';
+
+                            for (int k = j + 1; k < Number.Length; k++)
+                            {
+                                Number[k] = '0';
+                            }
+
+                            break;
+                        }
+                    }
+                    if (AddNum)
+                        BinNumList.Add(new StringBuilder(Number.ToString()));
+                    else
+                    {
+                        for (int z = 1; z < Number.Length; z++)
+                        {
+                            Number[z] = '0';
+                        }
+                        Number.Append("0");
+                        BinNumList.Add(new StringBuilder(Number.ToString()));
+
+                    }
+
+                }
+
+
+            }
+
+            return BinNumList;
+
+        }
+        static void Main(string[] args)
+        {
+            List<StringBuilder> BinNumsTo5 = GenerateBinNum(10);
+
+            Console.WriteLine(string.Join(", ", BinNumsTo5));
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ ProgrammingAdvices/23 Algorithms  Level 2/15 - Generate Binary Numbers/Generate Binary Nums Using For Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15___Generate_Binary_Numbers_Using_Queue
+{
+    internal class Program
+    {
+        public static List<string> GenerateBinNum(int MaxNumber)
+        {
+            Queue<string> BinNumbersQueueToGenerate = new Queue<string>();
+            List<string> BinNumbers = new List<string>();
+
+            BinNumbersQueueToGenerate.Enqueue("1");
+
+            for (int i = 1; i <= MaxNumber; i++)
+            {
+                string BinNumber = BinNumbersQueueToGenerate.Dequeue();
+                BinNumbers.Add(BinNumber);
+
+                BinNumbersQueueToGenerate.Enqueue(BinNumber + "0");
+                BinNumbersQueueToGenerate.Enqueue(BinNumber + "1");
+
+            }
+
+            return BinNumbers;
+        }
+
+        static void Main(string[] args)
+        {
+            var BinNumbers = GenerateBinNum(10);
+
+            Console.WriteLine("Binary Numbers from 0 To 10 :");
+            Console.WriteLine("[" + string.Join(", ", BinNumbers) + "]");
+        }
+    }
+  
+}
