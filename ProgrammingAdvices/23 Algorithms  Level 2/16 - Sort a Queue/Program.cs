e4eac6c021b28f30227dd09cec3d7f52e38196dd
mazinalnaimat <mazinalnaimat@gmail.com>
2025-12-20 10:36:57 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/16 - Sort a Queue/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16___Sort_a_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(5);
+            queue.Enqueue(1);   
+            queue.Enqueue(3);
+            queue.Enqueue(2);
+            queue.Enqueue(4);
+
+            Console.WriteLine("Input Queue : " 
+                                + string.Join(", " , queue) );
+
+            List<int> list = new List<int>(queue);
+
+            list.Sort();
+             queue = new Queue<int>(list);
+
+
+            Console.WriteLine("Sorted Queue: "
+                                + string.Join(", ", queue));
+
+
+
+        }
+    }
+}
---
+++ ProgrammingAdvices/23 Algorithms  Level 2/18 - Rotate a Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18___Rotate_a_Queue
+{
+    internal class Program
+    {
+        static Queue<int> RotateQueue(Queue<int> queue, int k)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                queue.Enqueue(queue.Dequeue());
+            }
+            return queue;
+        }
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
+            Queue<int> rotatedQueue = RotateQueue(queue, 2);
+            Console.WriteLine(string.Join(", ", rotatedQueue)); // Output: 3, 4, 5, 1, 2
+            Console.ReadKey();
+        }
+    }
+}
