bfcf90e76f40f458589b17115ba27681e520d725
mazinalnaimat <mazinalnaimat@gmail.com>
2026-01-10 17:11:38 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/50 - Find Numbers Disappeared in an Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50___Find_Numbers_Disappeared_in_an_Array
+{
+    internal class Program
+    {
+        static public List<int> FindNumsDisappearedInArray(in int[]Arr)
+        {
+            HashSet<int> set = new HashSet<int>(Arr);
+            List<int> NumsDisappearedInArray = new List<int>();
+            for(int i=1; i<=Arr.Max();i++)
+            {
+                if(!set.Contains(i))
+                    NumsDisappearedInArray.Add(i);
+            }
+
+            return NumsDisappearedInArray;
+        }
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[]{ 4, 3, 2, 7, 8, 2, 3, 1 };
+            Console.WriteLine("The input: " + string.Join(", ", Arr));
+
+            Console.WriteLine("Nums Disappeared In Array: " + string.Join(", ", FindNumsDisappearedInArray(Arr)));
+ 
+            Console.ReadLine();
+        }
+    }
+}
