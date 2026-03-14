5f1668bbf5f5350d3d216931354f6a265ab4b223
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-14 15:51:48 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/55 - Union of Two SortedSets/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _55___Union_of_Two_SortedSets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> Nums1 = new SortedSet<int> {1, 2, 3};
+            SortedSet<int> Nums2 = new SortedSet<int> {3, 4, 5};
+
+
+            Console.WriteLine("Nums1: " + String.Join(",", Nums1));
+            Console.WriteLine("Nums2: " + String.Join(",", Nums2));
+            Nums1.UnionWith(Nums2);
+            Console.WriteLine("Union Nums: " + String.Join(",", Nums1));
+
+            Console.ReadKey();
+        }
+    }
+}
