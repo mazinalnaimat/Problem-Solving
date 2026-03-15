7ee522085fac485ac2cf8adf4bd32ff502c425fe
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-15 15:53:43 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/60 - Find All Elements Not in a Range/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _60___Find_All_Elements_Not_in_a_Range
+{
+    internal class Program
+    {
+        static SortedSet<int> GetAllElmentsNotInRange(SortedSet<int> Nums, int Min, int Max)
+        {
+            SortedSet<int> Result = new SortedSet<int>(Nums);
+            SortedSet<int> Range = Nums.GetViewBetween(Min, Max);
+            Result.ExceptWith(Range);
+            return Result;
+        }
+        static void Main(string[] args)
+        {
+            SortedSet<int> Nums = new SortedSet<int>{1, 2, 3, 4, 5};
+            Console.WriteLine("Nums: " + String.Join(", ", Nums));
+            Nums = GetAllElmentsNotInRange(Nums, 2, 4);
+            Console.WriteLine("New Nums: " + String.Join(", ", Nums));
+            Console.ReadKey();
+        }
+    }
+}
