6f6008c92383ac0e5428d32b224ff289a571b795
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-15 15:59:46 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/61 - Find Count of Elements Less Than or Equal to a Value/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _61___Find_Count_of_Elements_Less_Than_or_Equal_to_a_Value
+{
+    internal class Program
+    {
+        static int GetElementsCountLessThanOrEqual(SortedSet<int> Nums, int Value)
+        {
+            return Nums.GetViewBetween(Nums.Min(), Value).Count();
+        }
+        static void Main(string[] args)
+        {
+            SortedSet<int> Nums = new SortedSet<int> { 1, 2, 3, 4, 5 };
+            int Value = 3;
+            Console.WriteLine("Nums: " + String.Join(", ", Nums));
+            Console.WriteLine("New Nums: " + String.Join(", ", Nums.GetViewBetween(Nums.Min(), Value)));
+            Console.WriteLine("New Nums Count: " + GetElementsCountLessThanOrEqual(Nums, Value));
+            Console.ReadKey();
+        }
+
+    }
+}
