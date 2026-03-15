3f7676749a63de4f486803379bcfcb4776a4ffe6
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-15 15:47:13 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/59 - Remove All Elements Within a Range/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _59___Remove_All_Elements_Within_a_Range
+{
+    internal class Program
+    {
+        static SortedSet<int> RemoveAllElementsInRange(SortedSet<int> Nums, int Min, int Max)
+        {
+
+           Nums.GetViewBetween(Min, Max).Clear();
+            return Nums;
+        }
+        static void Main(string[] args)
+        {
+            SortedSet<int> Nums = new SortedSet<int>{1, 2, 3, 4, 5};
+            Console.WriteLine("Nums: " + String.Join(", ", Nums));
+            Nums = RemoveAllElementsInRange(Nums, 2, 4);
+            Console.WriteLine("New Nums: " + String.Join(", ", Nums));
+            Console.ReadKey();
+        }
+    }
+}
