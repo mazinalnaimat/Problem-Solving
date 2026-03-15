aefacf40cf9b21d4fea4b505ce3554d822a4a94d
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-15 06:25:24 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/56 - Remove Elements Greater Than a Value/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _56___Remove_Elements_Greater_Than_a_Value
+{
+    internal class Program
+    {
+        static SortedSet<int>RemoveElementsGreaterThanValue(SortedSet<int> Nums, int Value)
+        {
+            return Nums.GetViewBetween(Nums.Min(), Value);
+        }
+        static void Main(string[] args)
+        {
+            SortedSet<int> Nums = new SortedSet<int> {1, 2, 3, 4, 5};
+            SortedSet<int> NewNums = new SortedSet<int>();
+            NewNums = RemoveElementsGreaterThanValue(Nums, 3);
+
+            Console.WriteLine("Nums: " + String.Join(", " ,Nums));
+            Console.WriteLine("New Nums: " + String.Join(", ", NewNums));
+            Console.ReadKey();
+        }
+    }
+}
