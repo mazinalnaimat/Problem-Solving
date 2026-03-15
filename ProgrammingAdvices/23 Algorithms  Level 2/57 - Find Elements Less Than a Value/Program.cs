33b1c9ba4aa597d814cd7bdf3a02a9f7bf361a7d
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-15 06:33:13 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/57 - Find Elements Less Than a Value/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _57___Find_Elements_Less_Than_a_Value
+{
+    internal class Program
+    {
+        static SortedSet<int>GetElementsLessThanValue(SortedSet<int> Nums, int Value)
+        {
+            return Nums.GetViewBetween(Nums.Min(), Value-1);
+        }
+        static void Main(string[] args)
+        {
+            SortedSet<int> Nums = new SortedSet<int>{1 ,2, 3, 4, 5};
+            SortedSet<int> NewNums = GetElementsLessThanValue(Nums, 4);
+
+            Console.WriteLine("Nums: " + String.Join(", ", Nums));
+            Console.WriteLine("New Nums: " + String.Join(", ", NewNums));
+            Console.ReadKey();
+
+        }
+    }
+}
