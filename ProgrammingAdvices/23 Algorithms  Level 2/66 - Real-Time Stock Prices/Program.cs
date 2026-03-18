38a859f7cbe5ae2b8d170613297192f967604f92
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-18 15:43:48 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/66 - Real-Time Stock Prices/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66___Real_Time_Stock_Prices
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ObservableCollection<string> stockPrices = new ObservableCollection<string>();
+            stockPrices.CollectionChanged += (sender, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                    Console.WriteLine($"New Stock Price Added: {e.NewItems[0]}");
+            };
+
+            stockPrices.Add("AAPL: 150.00");
+            stockPrices.Add("MSFT: 240.50");
+
+            Console.ReadKey();
+        }
+    }
+}
