29012cd0b30d90c580f00533bb2ac098b6a5a14b
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-18 22:43:33 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/69 - Dynamic Weather Updates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _69___Dynamic_Weather_Updates
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ObservableCollection<string> weatherUpdates = new ObservableCollection<string>();
+            weatherUpdates.CollectionChanged += (sender, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                    Console.WriteLine($"Weather Update: {e.NewItems[0]}");
+            };
+
+            weatherUpdates.Add("New York: Sunny, 25°C");
+            weatherUpdates.Add("London: Rainy, 15°C");
+
+            Console.ReadKey();
+        }
+    }
+}
