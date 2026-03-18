203675e20fb4dae2fb6730b1e2655b08fbfc1d49
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-18 22:45:16 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/70 - Real-Time Notification System/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _70___Real_Time_Notification_System
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ObservableCollection<string> notifications = new ObservableCollection<string>();
+            notifications.CollectionChanged += (sender, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                    Console.WriteLine($"New Notification: {e.NewItems[0]}");
+            };
+
+            notifications.Add("Your order is under processing.");
+            notifications.Add("Your order has been shipped.");
+            notifications.Add("Your order is Delivered.");
+            Console.ReadKey();
+        }
+    }
+}
