cfccf8e61cd4f42078a4984551b946dccfb62784
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-19 18:52:16 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/78 - Return Multiple Values from a Function/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _78___Return_Multiple_Values_from_a_Function
+{
+    internal class Program
+    {
+
+        static (string Name, int Age, double Grade) GetStudentInfo()
+        {
+            return ("Salim", 16, 92.5);
+        }
+        static void Main(string[] args)
+        {
+            var StudentInfo = GetStudentInfo();
+
+            Console.WriteLine($"Name: {StudentInfo.Name}, Age: {StudentInfo.Age}, Grade: {StudentInfo.Grade}");
+
+            Console.ReadKey();
+        }
+    }
+}
