424e00ead204e36b03c57d906f2857ee5a7aa429
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-19 18:47:33 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/76 - Survey Responses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _76___Survey_Responses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] ServeyResoposes = new string[5][];
+
+            ServeyResoposes[0] = new string[] { "Yes", "No", "Yes"};
+            ServeyResoposes[1] = new string[] { "Yes", "Yes"};
+            ServeyResoposes[2] = new string[] { "No", "No", "No"};
+            ServeyResoposes[3] = new string[] { "No", "Yes"};
+            ServeyResoposes[4] = new string[] { "Yes", "Yes", "Yes", "Yes", "No"};
+
+            Console.WriteLine("Servey Resposes:\n");
+            for (int i = 0; i < ServeyResoposes.Length; i++)
+            {
+                Console.Write("Servey " + (i + 1)+ ": ");
+                Console.WriteLine(String.Join(", ", ServeyResoposes[i]));
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProgrammingAdvices/23 Algorithms  Level 2/77 - City Temperature Data/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _77___City_Temperature_Data
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double[][] cityTemperatures = new double[2][];
+            cityTemperatures[0] = new double[] { 29.5, 30.0, 28.7 }; 
+            cityTemperatures[1] = new double[] { 25.0, 26.5 };       
+
+
+            Console.WriteLine("Temperature Data:");
+            for (int i = 0; i < cityTemperatures.Length; i++)
+            {
+                Console.Write($"City {i + 1}: ");
+                foreach (double temp in cityTemperatures[i])
+                {
+                    Console.Write(temp + "°C ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
