1411f4c9c4991775ab6420da0ee74bf2e60fa452
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-19 15:27:37 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/74 - Daily Sales for Different Products/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _74___Daily_Sales_for_Different_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] ProductsDailySales = new int[3][];
+
+            ProductsDailySales[0] = new int[] {12, 20, 40 ,13, 1, 5};
+            ProductsDailySales[1] = new int[] {8, 15, 90, 15, 3};
+            ProductsDailySales[2] = new int[] {10, 13 , 89, 34, 53, 23 ,92};
+
+            Console.WriteLine("Products Daily Sales:\n");
+            for(int i=0; i<ProductsDailySales.Length; i++)
+            {
+                Console.Write("Product" + (i + 1) + ": ");
+                Console.WriteLine(String.Join(", ", ProductsDailySales[i]));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
