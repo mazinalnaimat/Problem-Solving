ae4728c43bf7f049159cfe6805e6c5a42451fbaa
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-21 22:08:21 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/79 - Track Player Stats in a Game/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _79___Track_Player_Stats_in_a_Game
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var Player1 = (Name: "Mohammad", Health: 53,  Score: 12);
+            Console.WriteLine("Player1: " + Player1.Name + ", Health: " + Player1.Health + ", Score: " + Player1.Score);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProgrammingAdvices/23 Algorithms  Level 2/80 - Store and Compare Employee Details/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _80___Store_and_Compare_Employee_Details
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var employee1 = (Name: "Alice", Salary: 50000);
+            var employee2 = (Name: "Bob", Salary: 60000);
+
+            Console.WriteLine($"{employee1.Name} has {(employee1.Salary > employee2.Salary ? "higher" : "lower")} salary than {employee2.Name}");
+
+            Console.ReadKey();
+        }
+    }
+}
