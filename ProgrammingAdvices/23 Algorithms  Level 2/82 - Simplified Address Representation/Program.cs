f210115237ecafd79f1d5a71230aa35fb8d318d4
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-21 22:19:41 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/82 - Simplified Address Representation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _82___Simplified_Address_Representation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var address = (Street: "123 Main St", City: "Springfield", State: "IL", Zip: "62704");
+            Console.WriteLine($"Address: {address.Street}, {address.City}, {address.State}, {address.Zip}");
+            // Output: Address: 123 Main St, Springfield, IL, 62704
+            Console.ReadKey();
+        }
+    }
+}
