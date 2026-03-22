90630645b60479b8bd3d07f7c0b84486407668c4
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-22 21:47:24 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/85 - Optimizing Space in Large Data/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _85___Optimizing_Space_in_Large_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray Seats = new BitArray(1000);
+
+            Seats[0] = false;
+            Seats[1] = true;
+            Seats[2] = true;
+            Seats[3] = false;
+
+            Console.WriteLine($"{"Seat number",-15} {"Is free",-10}");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{' ',4}{i + 1,-8}{' ', 5}{Seats[i],-10}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
