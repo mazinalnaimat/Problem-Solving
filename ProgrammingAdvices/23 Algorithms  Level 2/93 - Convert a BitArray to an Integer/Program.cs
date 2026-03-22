5b71e758702fca9255fd0336ca42cae35d758f0c
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-22 22:41:05 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/93 - Convert a BitArray to an Integer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _93___Convert_a_BitArray_to_an_Integer
+{
+    internal class Program
+    {
+        static int ConvertBitArrayToInt(BitArray bits)
+        {
+            int Num = 0;
+            for (int i = 0; i < bits.Length; i++)
+            {
+                Num += (int)Math.Pow(2, i) * Convert.ToInt32(bits[i]);
+            }
+            return Num;
+        }
+        static void Main(string[] args)
+        {
+            BitArray bits1 = new BitArray(12);
+            bits1[2] = true;
+            bits1[6] = true;
+            bits1[10] = true;
+
+            Console.WriteLine("bits1 as int: " + ConvertBitArrayToInt(bits1));
+
+            Console.ReadKey();
+        }
+    }
+}
