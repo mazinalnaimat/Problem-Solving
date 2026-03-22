debb74c2e07ec829d2ba91f0b31afe32d4549092
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-22 22:13:34 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/89 -  Traffic Light Simulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _89____Traffic_Light_Simulation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            BitArray trafficLights = new BitArray(9); // 3 intersections, 3 lights each
+                                                      // Green at intersection 1
+            trafficLights[2] = true;
+
+
+            // Change light to Yellow
+            trafficLights[2] = false;
+            trafficLights[1] = true;
+
+
+            Console.WriteLine("Intersection 1 Lights:");
+            Console.WriteLine($"Red: {trafficLights[0]}, Yellow: {trafficLights[1]}, Green: {trafficLights[2]}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
