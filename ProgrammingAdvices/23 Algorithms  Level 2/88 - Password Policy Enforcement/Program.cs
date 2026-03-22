1fb1512ba84a75a701cff76dd22aad3f12cbaa68
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-22 22:11:33 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/88 - Password Policy Enforcement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _88___Password_Policy_Enforcement
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            BitArray passwordPolicy = new BitArray(4); // Uppercase, Lowercase, Digit, Special Character
+
+
+            string password = "Password123!";
+            passwordPolicy[0] = password.Any(char.IsUpper); // Uppercase
+            passwordPolicy[1] = password.Any(char.IsLower); // Lowercase
+            passwordPolicy[2] = password.Any(char.IsDigit); // Digit
+            passwordPolicy[3] = password.Any(ch => "!@#$%^&*".Contains(ch)); // Special Character
+
+
+            bool isValid = passwordPolicy.Cast<bool>().All(bit => bit);
+            Console.WriteLine($"Password Valid: {isValid}");
+            Console.ReadKey();
+
+        }
+    }
+}
