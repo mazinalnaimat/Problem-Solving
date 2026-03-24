e57b03df42d15fd47423d88b65fd5ab6f8ece466
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-24 19:56:34 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/96 - Sort and Remove Duplicates from a List/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _96___Sort_and_Remove_Duplicates_from_a_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Nums = new List<int> { 1, 2, 2, 4, 4, 5, 1, 5, 6, 2, 3, 5, };
+            HashSet<int> UnqNums = new HashSet<int>(Nums);
+            
+            Console.WriteLine("Nums:");
+            Console.WriteLine(String.Join(", " , Nums));
+            Console.WriteLine();
+
+            Console.WriteLine("UnqNums:");
+            Console.WriteLine(String.Join(", ", UnqNums));
+
+
+            Console.ReadKey();
+        }
+    }
+}
