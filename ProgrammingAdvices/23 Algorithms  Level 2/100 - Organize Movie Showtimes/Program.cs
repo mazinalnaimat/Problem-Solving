ab2040217514e649b6f8401a2d1d32e2b985f56b
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-25 22:58:27 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/100 - Organize Movie Showtimes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100___Organize_Movie_Showtimes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SortedSet<DateTime> showtimes = new SortedSet<DateTime>
+        {
+            new DateTime(2024, 11, 19, 14, 0, 0),
+            new DateTime(2024, 11, 19, 12, 30, 0),
+            new DateTime(2024, 11, 19, 16, 15, 0)
+        };
+
+            Console.WriteLine("Next showtime: " + showtimes.Min);
+            Console.WriteLine("All showtimes:");
+            foreach (var time in showtimes)
+            {
+                Console.WriteLine(time.ToShortTimeString());
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProgrammingAdvices/23 Algorithms  Level 2/99 - Automatically Sort Event Timelines/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99___Automatically_Sort_Event_Timelines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SortedSet <DateTime> Events = new SortedSet <DateTime> ();
+
+            Events.Add(new DateTime(2026, 8, 10, 12, 25, 00));
+            Events.Add(new DateTime(2026, 12, 12, 13, 10, 00));
+            Events.Add(new DateTime(2026, 9, 21, 15, 51, 00));
+
+            Console.WriteLine("Events:");
+            Console.WriteLine(String.Join("\n", Events));
+
+            Console.ReadKey();
+        }
+    }
+}
