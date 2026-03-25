cc45634d5592539f21ab8db6f626425feb10a335
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-25 22:48:53 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/98 - Track Unique Active Users by Login Time/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _98___Track_Unique_Active_Users_by_Login_Time
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            SortedSet<DateTime> activeUsers = new SortedSet<DateTime>();
+
+            // Simulate user login times
+            activeUsers.Add(new DateTime(2024, 11, 19, 10, 0, 0));
+            activeUsers.Add(new DateTime(2024, 11, 19, 10, 15, 0));
+            activeUsers.Add(new DateTime(2024, 11, 19, 10, 10, 0));
+
+            Console.WriteLine("Active users login times (sorted):");
+            foreach (var time in activeUsers)
+            {
+                Console.WriteLine(time);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
