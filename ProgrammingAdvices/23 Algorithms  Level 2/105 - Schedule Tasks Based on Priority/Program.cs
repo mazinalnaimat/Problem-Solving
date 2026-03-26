3508441885e68d8926d22f17c3846c9f7dc0ec09
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-26 21:29:36 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/105 - Schedule Tasks Based on Priority/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _105___Schedule_Tasks_Based_on_Priority
+{
+    internal class Program
+    {
+
+        public class Task : IComparable<Task>
+        {
+
+            public string Name { get; set; }
+            public int Priority { get; set; }
+
+            public Task(string Name, int Priority)
+            {
+                this.Name = Name;
+                this.Priority = Priority;
+            }
+
+            public int CompareTo(Task Other)
+            {
+
+                int result = this.Priority.CompareTo(Other.Priority);
+                if (result == 0)
+                {
+                    result = this.Name.CompareTo(Other.Name);
+                }
+                return result;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            SortedSet<Task> Tasks = new SortedSet<Task>();
+
+            Tasks.Add(new Task("Edge", 1));
+            Tasks.Add(new Task("Chrome", 5));
+            Tasks.Add(new Task("Firefox", 3));
+            Tasks.Add(new Task("VSC", 2));
+            Tasks.Add(new Task("OBS", 2));
+
+            Console.WriteLine("Tasks:");
+
+            int Counter = 0;
+            foreach (Task T in Tasks)
+            {
+                Counter++;
+                Console.WriteLine("Task " + Counter + ": " + T.Name + ", Pri:" + T.Priority);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
