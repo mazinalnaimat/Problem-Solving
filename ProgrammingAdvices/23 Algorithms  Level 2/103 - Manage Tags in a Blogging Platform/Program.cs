a4e38494ef83126dc2852b895c6c2b4e91b75bf4
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-26 21:14:27 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/103 - Manage Tags in a Blogging Platform/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _103___Manage_Tags_in_a_Blogging_Platform
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            SortedSet<string> tags = new SortedSet<string>
+        {
+            "C#", "Programming", "Tutorial", "DotNet"
+        };
+
+            // Add more tags
+            tags.Add("Coding");
+            tags.Add("Programming"); // Duplicate, won't be added
+
+            Console.WriteLine("All Tags:");
+            foreach (var tag in tags)
+            {
+                Console.WriteLine(tag);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
