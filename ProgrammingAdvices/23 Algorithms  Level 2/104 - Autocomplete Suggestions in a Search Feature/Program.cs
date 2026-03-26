5e1e53caaf297b904755809ea2a545938ff6044d
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-26 21:16:36 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/104 - Autocomplete Suggestions in a Search Feature/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _104___Autocomplete_Suggestions_in_a_Search_Feature
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SortedSet<string> words = new SortedSet<string>
+        {
+            "apple", "application", "appreciate", "banana", "band", "bandwidth", "cat", "cater"
+        };
+
+            Console.Write("Enter a prefix: ");
+            string prefix = Console.ReadLine();
+
+            var suggestions = GetSuggestions(words, prefix);
+
+            Console.WriteLine("Suggestions:");
+            foreach (var word in suggestions)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static IEnumerable<string> GetSuggestions(SortedSet<string> words, string prefix)
+        {
+            foreach (var word in words)
+            {
+                if (word.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                {
+                    yield return word;
+                }
+            }
+        }
+    }
+}
