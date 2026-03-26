3e9a5c68a2fd0b0cee76275044319d3c8afaceaf
mazinalnaimat <mazinalnaimat@gmail.com>
2026-03-26 21:12:48 +0300

    Add files via upload

---
+++ ProgrammingAdvices/23 Algorithms  Level 2/101 - Manage Meeting Times for a Calendar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _101___Manage_Meeting_Times_for_a_Calendar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<TimeSpan> MeetingTimes = new SortedSet<TimeSpan>();
+
+            MeetingTimes.Add(new TimeSpan(14, 15, 0));
+            MeetingTimes.Add(new TimeSpan(13, 30, 0));
+            MeetingTimes.Add(new TimeSpan(10, 15, 0));
+            MeetingTimes.Add(new TimeSpan(14, 40, 0));
+
+            Console.WriteLine("Meetings:");
+            Console.WriteLine(String.Join("\n", MeetingTimes));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ProgrammingAdvices/23 Algorithms  Level 2/102 -   Manage Unique Flight Times - Solution/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _102_____Manage_Unique_Flight_Times___Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<TimeSpan> FlightTimes = new SortedSet<TimeSpan>();
+
+            FlightTimes.Add(new TimeSpan(12, 20, 0));
+            FlightTimes.Add(new TimeSpan(10, 45, 0));
+            FlightTimes.Add(new TimeSpan(11, 10, 0));
+            FlightTimes.Add(new TimeSpan(11, 10, 0));
+
+            Console.WriteLine("Flight Times:");
+            Console.WriteLine(String.Join("\n", FlightTimes));
+
+            Console.ReadKey();
+
+        }
+    }
+}
