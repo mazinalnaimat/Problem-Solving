f0c1e07322d6a18116acc6c40d3a9d75479b2c85
Mazin Al-Naimat <mazinalnaimat@gmail.com>
2026-06-16 11:53:57 +0300

    V2 File Monitoring WinSer Add Console Interactive and more error handling

---
+++ ProgrammingAdvices/24 Windows Services/1 Project  - File Monitoring Windows Service/V2/ProjectInstaller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace _1_Project____File_Monitoring_Windows_Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+         
+        private ServiceProcessInstaller serviceProcessInstaller;
+        private ServiceInstaller serviceInstaller;
+
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            serviceProcessInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem
+            };
+
+            // Configure the Service Installer
+            serviceInstaller = new ServiceInstaller
+            {
+                ServiceName = "FileMonitoringWindService",
+                DisplayName = "File Monitoring Windows Service",
+                StartType = ServiceStartMode.Manual
+            };
+
+            Installers.Add(serviceProcessInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
+
+
+
