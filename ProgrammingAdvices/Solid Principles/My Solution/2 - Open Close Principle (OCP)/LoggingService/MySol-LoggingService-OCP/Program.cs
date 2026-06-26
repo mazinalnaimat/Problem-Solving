cddae4011fc2b4419a03c0564363c4382b0a5c10
Mazin Al-Naimat <mazinalnaimat@gmail.com>
2026-06-26 15:48:32 +0300

    Add 2nd Solid Principles (OCP) Sols

---
+++ ProgrammingAdvices/Solid Principles/My Solution/2 - Open Close Principle (OCP)/LoggingService/MySol-LoggingService-OCP/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Transactions;
+
+public class LoggingService
+{
+    private ILogging _logging;
+    public LoggingService(ILogging loggingService)
+    {
+        _logging = loggingService;
+    }
+    public void Log(string Msg)
+    {
+        _logging.Log(Msg);
+    }
+
+}
+
+public interface ILogging
+{
+    public void Log(string Msg);
+}
+
+public class FileLoggingService : ILogging
+{
+    // Method to log to file
+    public  void Log(string Msg)
+    {
+        Console.WriteLine($"\nLog to file: {Msg}");
+    }
+}
+
+public class EventLogService : ILogging
+{
+    // Method to log to EventLog
+    public void Log(string Msg)
+    {
+        Console.WriteLine($"\nLog to Event Log: {Msg}");
+    }
+}
+
+public class DatabaseLoggingService : ILogging
+{
+
+    // Method to log to file
+    public  void Log(string Msg)
+    {
+        Console.WriteLine($"\nLog to Database: {Msg}");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        LoggingService FileLoggingService = new LoggingService(new FileLoggingService());
+        // Log to File
+        FileLoggingService.Log("Error Occured line xxx.");
+
+
+        LoggingService EventLogService = new LoggingService(new EventLogService());
+        // Log to Event Log
+        EventLogService.Log("Error Occured line xxx.");
+
+        LoggingService DatabaseLoggingService = new LoggingService(new DatabaseLoggingService());
+        // Log to Database
+        DatabaseLoggingService.Log("Error Occured line xxx.");
+
+
+
+
+        Console.ReadKey();
+
+    }
+}
---
+++ ProgrammingAdvices/Solid Principles/My Solution/2 - Open Close Principle (OCP)/PaymentService/MySol-PaymentService-OCP/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+using System;
+
+public class PaymentService
+{
+    private IPayment _payment;
+
+    public PaymentService(IPayment payment)
+    {
+        _payment = payment;
+    }
+
+    public void Pay(decimal Price)
+    {
+        _payment.Pay(Price);
+    }
+}
+
+
+public interface IPayment
+{
+    void Pay(decimal Price);
+}
+
+public class PayPalPaymentService : IPayment
+{
+    public void Pay(decimal Price)
+    {
+        Console.WriteLine($"Pay ${Price} with PayPal\n");
+    }
+}
+public class CreditCardPaymentService : IPayment
+{
+    public void Pay(decimal Price)
+    {
+        Console.WriteLine($"Pay ${Price} with Credit Card\n");
+    }
+}
+public class BankTransferPaymentService: IPayment
+{
+    public void Pay(decimal Price)
+    {
+        Console.WriteLine($"Pay ${Price} with Bank Transfer\n");
+    }
+}
+public class BitcoinPaymentService : IPayment
+{
+    public void Pay(decimal Price)
+    {
+        Console.WriteLine($"Pay ${Price} with Bitcoin\n");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        PaymentService paymentService = new PaymentService(new PayPalPaymentService());
+        paymentService.Pay(500);
+
+        paymentService = new PaymentService(new CreditCardPaymentService());
+        paymentService.Pay(500);
+
+        paymentService = new PaymentService(new BankTransferPaymentService());
+        paymentService.Pay(500); 
+
+        paymentService = new PaymentService(new BitcoinPaymentService());
+        paymentService.Pay(500);
+
+        Console.ReadKey();
+    }
+}
