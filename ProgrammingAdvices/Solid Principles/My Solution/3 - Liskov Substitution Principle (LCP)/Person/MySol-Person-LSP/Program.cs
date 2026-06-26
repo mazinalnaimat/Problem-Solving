459d0796de1f1d6c1bb0b93d55a0fb96e38a6e0a
Mazin Al-Naimat <mazinalnaimat@gmail.com>
2026-06-26 15:49:45 +0300

    add 3rd Solid Principle (LSP) Sols

---
+++ ProgrammingAdvices/Solid Principles/My Solution/3 - Liskov Substitution Principle (LCP)/Person/MySol-Person-LSP/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public class Person
+{
+
+    // Virtual method to send an email
+    public virtual void SendEmail()
+    {
+        Console.WriteLine("Sending email to a person...");
+    }
+
+
+}
+
+public class Employee : Person
+{
+    // Override PaySalary for employees
+    public virtual void PaySalary()
+    {
+        Console.WriteLine("Paying salary to an employee...");
+    }
+
+    // Override SendEmail for employees
+    public override void SendEmail()
+    {
+        Console.WriteLine("Sending email to an employee...");
+    }
+}
+
+public class Manager : Employee
+{
+    // Override PaySalary for managers
+    public override void PaySalary()
+    {
+        Console.WriteLine("Paying salary to a manager...");
+    }
+
+    // Override SendEmail for managers
+    public override void SendEmail()
+    {
+        Console.WriteLine("Sending email to a manager...");
+    }
+}
+
+public class Student : Person
+{
+
+    // Override SendEmail for students
+    public override void SendEmail()
+    {
+        Console.WriteLine("Sending email to a student...");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee employee1 = new Employee();
+        employee1.PaySalary();   // Calls PaySalary method for Employee
+        employee1.SendEmail();   // Calls SendEmail method for Employee
+
+        Employee employee2 = new Manager();
+        employee2.PaySalary();   // Calls PaySalary method for Manager
+        employee2.SendEmail();   // Calls SendEmail method for Manager
+
+        Person person3 = new Student();
+        person3.SendEmail();   // Calls SendEmail method for Student
+
+
+        Console.ReadKey();
+    }
+}
---
+++ ProgrammingAdvices/Solid Principles/My Solution/3 - Liskov Substitution Principle (LCP)/Vehicle/MySol-Vehicle-LSP/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Vehicle
+{
+
+    public virtual void Drive()
+    {
+        Console.WriteLine("Driving...");
+    }
+
+}
+
+public class VehicleWithEngine : Vehicle
+{
+    public virtual void StartEngine()
+    {
+        Console.WriteLine("Starting engine...");
+    }
+
+}
+public class Car : VehicleWithEngine
+{
+    public override void StartEngine()
+    {
+        Console.WriteLine("Car engine started...");
+    }
+
+    public override void Drive()
+    {
+        Console.WriteLine("Car is driving...");
+    }
+}
+
+public class Bicycle : Vehicle
+{
+
+    public override void Drive()
+    {
+        Console.WriteLine("Bicycle is riding...");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        VehicleWithEngine vehicle1 = new Car();
+        vehicle1.StartEngine();
+        vehicle1.Drive();
+
+        Vehicle vehicle2 = new Bicycle();
+        vehicle2.Drive();
+
+        Console.ReadKey();
+    }
+}
