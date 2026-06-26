eed1162a6b53da8ecdadce4aa208cfdbd62e0a55
Mazin Al-Naimat <mazinalnaimat@gmail.com>
2026-06-26 15:50:42 +0300

    add 4th Solid Principle (ISP) Sols

---
+++ ProgrammingAdvices/Solid Principles/My Solution/4 - Interface Segregation Principle (ISP)/Device/MySol-Device-ISP/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+public interface IEmailDevice
+{
+    void SendEmail();
+
+}
+public interface ICallDevice
+{
+    void MakeCall();
+
+}
+public interface IGPSDevice
+{
+    void UseGPS();
+
+}
+public interface IPhotoDevice
+{
+    void TakePhoto();
+
+}
+
+
+
+public class Smartphone : IEmailDevice, ICallDevice, IGPSDevice, IPhotoDevice
+{
+    public void MakeCall()
+    {
+        Console.WriteLine("Making a call.");
+    }
+
+    public void TakePhoto()
+    {
+        Console.WriteLine("Taking Photo.");
+    }
+
+    public void SendEmail()
+    {
+        Console.WriteLine("Sending an email.");
+    }
+
+    public void UseGPS()
+    {
+        Console.WriteLine("Using GPS.");
+    }
+}
+
+public class Computer : IEmailDevice
+{
+
+
+    public void SendEmail()
+    {
+        Console.WriteLine("Sending an email.");
+    }
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Smartphone smartphone = new Smartphone();
+        Console.WriteLine("SmartPhone:");
+        smartphone.MakeCall();
+        smartphone.TakePhoto();
+        smartphone.SendEmail();
+        smartphone.UseGPS();
+
+        Computer computer = new Computer();
+        Console.WriteLine("\nComputer:");
+        computer.SendEmail();
+        //computer.TakePhoto();// This will throw an exception.
+        //computer.SendEmail();// This will throw an exception.
+        //computer.UseGPS();// This will throw an exception.
+        
+
+        Console.ReadKey();
+
+        
+    }
+}
---
+++ ProgrammingAdvices/Solid Principles/My Solution/4 - Interface Segregation Principle (ISP)/Payment/MySol-Payment-ISP/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public interface IPayment
+{
+    void Pay();
+}
+public interface ICreditCard: IPayment
+{
+}
+public interface IPayPal: IPayment
+{
+
+}
+public interface IBitcoin: IPayment
+{
+
+}
+
+
+public class CreditCardPayment : ICreditCard
+{
+    public void Pay()
+    {
+        Console.WriteLine("Payment with credit card.");
+    }
+
+}
+
+public class PayPalPayment : IPayPal
+{
+
+    public void Pay()
+    {
+        Console.WriteLine("Payment with PayPal.");
+    }
+
+}
+public class BitcoinPayment : IBitcoin
+{
+
+    public void Pay()
+    {
+        Console.WriteLine("Payment with Bitcoin.");
+    }
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        IPayment creditCardPayment = new CreditCardPayment();
+        creditCardPayment.Pay();
+
+
+        IPayment payPalPayment = new PayPalPayment();
+        payPalPayment.Pay();
+
+        Console.ReadKey();
+
+    }
+}
